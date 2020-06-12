9fe5f1f3f1c9e48ebb734bff6eb11cfe6fecf6b0
taeyoungs <xoxodudwkd@gmail.com>
2020-06-12 14:34:33 +0900

    Create Users model
    Add custom profile field (admin paenl)

--- users/models.py
+++ users/models.py
@@ -4,22 +4,32 @@ from django.db import models
 
 class User(AbstractUser):
 
+    """ Custom User Model """
+
     GENDER_MALE = "male"
     GENDER_FEMALE = "female"
     GENDER_OTHER = "other"
 
-    GENDER_CHOICES = [
+    GENDER_CHOICES = (
         (GENDER_MALE, "Male"),
         (GENDER_FEMALE, "Female"),
         (GENDER_OTHER, "Other"),
-    ]
-
-    avatar = models.ImageField(null=True, blank=True)
-    gender = models.CharField(
-        null=True,
-        max_length=10,
-        blank=True,
-        choices=GENDER_CHOICES,
-        default=GENDER_MALE,
     )
-    bio = models.TextField(default="", blank=True)
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+
+    LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, "en"), (LANGUAGE_KOREAN, "kr"))
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
+
+    avatar = models.ImageField(blank=True)
+    gender = models.CharField(max_length=10, blank=True, choices=GENDER_CHOICES)
+    bio = models.TextField(blank=True)
+    birthdate = models.DateField(null=True, blank=True)
+    language = models.CharField(max_length=2, choices=LANGUAGE_CHOICES, blank=True)
+    currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, blank=True)
+    superhost = models.BooleanField(default=False)
