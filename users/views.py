7a43616a6472b238b17090654f251b54018f48da
taeyoungs <xoxodudwkd@gmail.com>
2020-06-27 17:17:29 +0900

    Change LoginView using FormView
    Fixed Error using reverse_lazy - don't have any urlpatterns

--- users/views.py
+++ users/views.py
@@ -1,16 +1,27 @@
+from django.views.generic import FormView
+from django.views import View
+from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
-from django.views import View
 from . import forms
 
 
-class LoginView(View):
-    def get(self, request):
+class LoginView(FormView):
 
-        form = forms.LoginForm(request.GET)
+    template_name = "users/login.html"
+    form_class = forms.LoginForm
+    success_url = reverse_lazy("core:home")
 
-        return render(request, "users/login.html", {"form": form})
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
 
+        return super().form_valid(form)
+
+    """
     def post(self, request):
 
         form = forms.LoginForm(request.POST)
@@ -24,6 +35,7 @@ class LoginView(View):
                 return redirect(reverse("core:home"))
 
         return render(request, "users/login.html", {"form": form})
+    """
 
 
 def log_out(request):
