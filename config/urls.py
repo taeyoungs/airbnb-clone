64803a15bc67bad54d3c30d8cc38cbd22e9ca014
taeyoungs <xoxodudwkd@gmail.com>
2020-06-27 16:38:54 +0900

    Add Login, Logout

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path("", include("core.urls", namespace="core"),),
     path("rooms/", include("rooms.urls", namespace="rooms")),
+    path("users/", include("users.urls", namespace="users")),
     path("admin/", admin.site.urls),
 ]
 
