43e177d7cf9b3b0b4e897706f0b36e6b60350ec2
taeyoungs <xoxodudwkd@gmail.com>
2020-06-29 14:39:04 +0900

    Add Sign Up nav
    Create SignUpView, SignUpForm using FormView

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 app_name = "users"
 
-urlpatterns = [path("login/", views.LoginView.as_view(), name="login"), path("logout/", views.log_out, name="logout")]
+urlpatterns = [
+    path("login/", views.LoginView.as_view(), name="login"),
+    path("logout/", views.log_out, name="logout"),
+    path("signup/", views.SignUpView.as_view(), name="signup"),
+]
