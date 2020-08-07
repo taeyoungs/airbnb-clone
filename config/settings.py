17799513465f0497c04d045a10ec4ffefe865607
taeyoungs <xoxodudwkd@gmail.com>
2020-08-07 16:13:10 +0900

    Move django secret to env file

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = [os.environ.get("DJANGO_SECRET"), "bfVhfDWRMnF7Cu!uw@EVyEooF&TPUt9jfgGJW5"]
+SECRET_KEY = os.environ.get("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG"))
